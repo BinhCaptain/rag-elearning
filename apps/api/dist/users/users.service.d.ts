6327c86467e4cb6ccf124c2ed1f67b4a24102d2a
phucnhan <phucnhan0406@gmail.com>
2026-03-17 22:17:53 +0700

    fix and update

--- apps/api/dist/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, User } from '@prisma/client';
-export declare class UsersService {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    findOneByEmail(email: string): Promise<User | null>;
-    findOneById(id: string): Promise<Omit<User, 'passwordHash'> | null>;
-    create(data: Prisma.UserCreateInput): Promise<User>;
-}
